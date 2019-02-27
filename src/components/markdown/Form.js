7bb406a652e577c99027544c2db873e1de5ce400
Paige Gorry <paigeegorry@gmail.com>
2019-02-27 15:24:12 -0800

    created static form - switched to new branch for redux hookup (#4)
    * markdown action test passing?
    
    * update new markdown is working, can see in markdown state
    
    * cleaned up maybe
    
    * changed spacing so we can pr
    
    * got rid of create new markdown, added update markdown test
    
    * added travis
    
    * added reducer test, its passing
    
    * added selector test for getMarkdown, its passing
    
    * test pass when we fixed document and update state
    
    * added form
    
    * created Markdown add form

---
+++ src/components/markdown/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <form>
+      Title: <input type="text" name="title" />
+      <button>Add Markdown</button>
+    </form>
+  );
+}
+ 
+export default Form;
--- src/container/Document.js
+++ src/container/Document.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
-import Preview from './Preview';
-import Editor from './Editor';
+import Preview from '../components/markdown/Preview';
+import Editor from '../components/markdown/Editor';
 import styles from './Document.css';
-import store from '../../store';
-import { getMarkdown } from '../../selectors/markdownSelectors';
-import { updateMarkdown } from '../../actions/markdownActions';
+import store from '../store';
+import { getMarkdown } from '../selectors/markdownSelectors';
+import { updateMarkdown } from '../actions/markdownActions';
+import Form from '../components/markdown/Form';
 
 export default class Document extends PureComponent {
   state = {
@@ -38,6 +39,7 @@ export default class Document extends PureComponent {
     const { markdown } = this.state;
     return (
       <>
+      <Form />
       <div className={styles.Document}>
         <Editor markdown={markdown} updateMarkdown={this.updateMarkdown} />
         <Preview markdown={markdown} />
