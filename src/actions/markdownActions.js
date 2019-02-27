ede758f5ed909d701ccd8b1d08552c98c94656d1
Kristin Hortsch <kristinhortsch@gmail.com>
2019-02-27 08:41:23 -0800

    changed spacing so we can pr

--- src/actions/markdownActions.js
+++ src/actions/markdownActions.js
@@ -1,12 +1,10 @@
 export const CREATE_MARKDOWN = 'CREATE_MARKDOWN';
-
 export const createMarkdown = markdown => ({
   type: CREATE_MARKDOWN,
   payload: { markdown }
 });
 
 export const NEW_MARKDOWN_UPDATE = 'NEW_MARKDOWN_UPDATE';
-
 export const updateNewMarkdown = markdown => ({
   type: NEW_MARKDOWN_UPDATE,
   payload: { markdown }
