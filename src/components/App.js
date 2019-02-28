a17df029527a2c66142504141c48add4ce4f085c
Dyanna T <39032450+deetolman@users.noreply.github.com>
2019-02-28 14:37:05 -0800

    Styled landing page (#9)
    * router paths working for markdowns and home page!
    
    * added links between home and documentView and they work
    
    * styled landing page and added description
    
    * trimmed the fat
    
    * took out white space

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Document from '../container/Document';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import 'normalize.css';
 
 export default function App() {
   return (
     <>
-      <h1>Markdown Editor</h1>
-      <Document />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/markdown" component={Document} />
+        <Route exact path="/markdown/:title" component={Document} />
+      </Switch>
+    </Router>
     </>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
+
+function Home() {
+  return (
+    <div className={styles.home}>
+      <nav>
+        <Link to="/markdown">
+      Create a markdown
+        </Link>
+      </nav>
+      <h1>MARKDOWNLY</h1>
+      <p>Welcome to an interactive markdown editor where you can create and edit markdown files and see a preview </p>
+    </div>
+  );
+}
+
+export default Home;
