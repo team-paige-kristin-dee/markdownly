90c2e38e5332a589ccba12864f643532ed512a88
Paige Gorry <paigeegorry@gmail.com>
2019-02-28 13:53:23 -0800

    router paths working for markdowns and home page!

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Document from '../container/Document';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
 
 export default function App() {
   return (
     <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/markdown" component={Document} />
+      </Switch>
+    </Router>
       <h1>Markdown Editor</h1>
+      <p>This is App</p>
       <Document />
     </>
   );
